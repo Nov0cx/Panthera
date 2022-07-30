e9e2c5f5fe5cf66982801b09381963b87170deb6
Nov0cx <68678245+Nov0cx@users.noreply.github.com>
2022-07-30 08:15:00 +0200

    Changed RenderAPI

--- Panthera-Core/src/Panthera/Render/RenderAPI.hpp
+++ Panthera-Core/src/Panthera/Render/RenderAPI.hpp
@@ -1,9 +1,23 @@
 #ifndef PANTHERA_RENDERAPI_HPP
 #define PANTHERA_RENDERAPI_HPP
 
+#include <cinttypes>
+
 namespace Panthera
 {
+    // Want to support all of them.
+    enum class RendererAPI
+    {
+        NONE = 0,
+        OpenGL = 1,
 
+        Vulkan = 2,
+        Direct3D = 3,
+        Metal = 4,
+        OpenGLES = 5,
+    };
+
+    using RendererID = std::uint32_t;
 }
 
 #endif //PANTHERA_RENDERAPI_HPP
