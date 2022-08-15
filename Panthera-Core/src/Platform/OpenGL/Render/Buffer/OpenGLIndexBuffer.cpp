b986d84fd3959d8c6fd84ac28e79893aa0dfc47c
Nov0cx <68678245+Nov0cx@users.noreply.github.com>
2022-08-15 08:24:03 +0200

    gl4 for IndexBuffers

--- Panthera-Core/src/Platform/OpenGL/Render/Buffer/OpenGLIndexBuffer.cpp
+++ Panthera-Core/src/Platform/OpenGL/Render/Buffer/OpenGLIndexBuffer.cpp
@@ -8,7 +8,7 @@ namespace Panthera
     OpenGLIndexBuffer::OpenGLIndexBuffer(const void *indicies, uint32_t count)
         : m_Count(count)
     {
-        glGenBuffers(1, &m_RendererID);
+        glCreateBuffers(1, &m_RendererID);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indicies, GL_STATIC_DRAW);
     }
