2724f4e55286e3824a135d37f53a183ce6681d90
Nov0cx <niklas.leygraf44@gmail.com>
2022-09-18 13:07:15 +0200

    Pair

---
+++ Panthera-Core/src/Panthera/Core/Pair.hpp
@@ -0,0 +1,14 @@
+#ifndef PANTHERA_PAIR_HPP
+#define PANTHERA_PAIR_HPP
+
+namespace Panthera
+{
+    template<typename T, typename U>
+    struct Pair
+    {
+        T first;
+        U second;
+    };
+}
+
+#endif //PANTHERA_PAIR_HPP
